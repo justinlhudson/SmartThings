07a5bca50f74321588bd00b6bff6b8ace3b6ba7a
Justin L Hudson <justinlhudson@gmail.com>
2015-02-08 15:21:00 -0500

    ModeWatcher app added, see read

---
+++ ModeWatcher.groovy
@@ -0,0 +1,99 @@
+definition(
+    name: "ModeWatcher",
+    namespace: "Operations",
+    author: "justinlhudson",
+    description: "Changing mode on zones (i.e. Zone 1 -> Zone 2 => New Mode).",
+    category:  "Safety & Security",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+preferences {
+    section("Zones Mode") {
+        for (int n = 1; n <= 2; n++) {
+            input "zone_${n}", "capability.motionSensor", title:"Zone ${n}", multiple:true, required:true
+        }
+        input "setMode", "mode", title:"Mode Revert", multiple:false, required:true
+        input "window", "number", title:"Window period (seconds)", defaultValue:10
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    unschedule()
+
+    initialize()
+}
+
+def modeHandler(evt)
+{
+    state.prevMode = state.currentMode
+    state.currentMode = evt.value
+
+    log.debug "Prev. Mode: ${state.prevMode}" 
+    log.debug "Current Mode: ${state.currentMode}"
+/*
+    if (location.mode == "Away") {
+
+    }
+    else if (location.mode == "Night") {
+
+    }
+    else {
+
+    }
+*/
+}
+
+def zone_2_Handler(evt)
+{
+    log.debug "Z2: ${evt.value}"
+    if(state.zone_1  > 0 && evt.value == "active") {
+        state.zone_2 = state.zone_2 + 1
+    }
+}
+
+def zone_1_Handler(evt)
+{
+    log.debug "Z1: ${evt.value}"
+    if( evt.value == "active") {
+        state.zone_1 = state.zone_1 + 1
+    }
+    else {
+        def now = new Date()
+        def runTime = new Date((new Date()).getTime() + (settings.window * 1000))
+        runOnce(runTime, operation, [overwrite: true])
+    }
+}
+
+def operation()
+{
+    if(state.zone_1 > 0 && state.zone_2 <= 0) {
+        if(state.prevMode == settings.setMode) {  // if prev mode is mode we want
+            if(state.currentMode != settings.setMode) {
+                setLocationMode(settings.setMode)
+                log.debug "Revert Mode: ${settings.setMode}"  //note: mode not change instantly so false reading
+            }
+        }
+    }
+
+    reset()
+}
+
+private def reset()
+{
+    state.zone_1 = 0
+    state.zone_2 = 0
+}
+
+private def initialize() {
+    reset()
+
+    subscribe(location, "mode", modeHandler)
+
+    subscribe(zone_1, "motion", zone_1_Handler)
+    subscribe(zone_2, "motion", zone_2_Handler)
+}
--- README.md
+++ README.md
@@ -5,6 +5,16 @@ SmartThings Development
 
 Resets alarm if active on next cycle of interval, incase was thrown just before interval makes sure full cycle has alarm.  Can setup to still strobe...
 
+## ModeWatcher
+
+Watches for zone 1 entry into zone 2 and if no entry into zone 2 will revert back to location mode before zone 1 was entered.  
+
+# Story
+
+If Night mode changes to Home mode with zone 1 sensing presence (making the transition). Yet zone 2 in Night mode is sensing intruder.  If zone 1 goes active changing location mode, but zone 2 is not passed within window peroid, will revert to location mode prior to zone 1 presence.  
+
+This solves my problem where bedroom hallway will change location mode to Home (so can take dog out).  However, if just going to bathroom and back want location mode to go back quickly if never actually went downstairs (to take dog out).
+
 ## Virtual Devices
 
 Lets on set switches and contacts without actually having any for controlling other application operations states.
